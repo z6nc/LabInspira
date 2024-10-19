7923180561c60ee1648145c743ff1605761608a8
z6nc <andresvelasquezarotinco@gmail.com>
2024-10-19 18:08:48 -0500

    Update navbar logo and styling

--- src/scripts/proyect.ts
+++ src/scripts/proyect.ts
@@ -1,25 +1,37 @@
 export const Proyectos = [
     {
         id: 1,
-        nombre: 'TermoConvert',
+        nombre: 'Termo',
+        secondNombre: 'Convert',
         descripcion: 'Convierte temperaturas entre Celsius y Fahrenheit de forma rápida y sencilla.',
         imagen: 'https://tse1.mm.bing.net/th?id=OIG2..PVDNQhsvuB6kDOBtH4d&pid=ImgGn',
         link: '/converterTemperatura',
-    },
+        svg: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">'
+           + '<path d="M17.25 8.52V5A5.25 5.25 0 0 0 7.5 5v3.52A7.5 7.5 0 1 0 17.25 8.52zM12 22.5a6 6 0 0 1-3.65-10.71 1.5 1.5 0 0 0 .65-1.22V5a3 3 0 1 1 6 0v5.57c0 .48.24.94.65 1.22A6 6 0 0 1 12 22.5zM11.25 11V7.5a.75.75 0 1 1 1.5 0V11a.75.75 0 1 1-1.5 0z"/>'
+           + '</svg>',
+      },
     {
         id: 2,
-        nombre: 'Testimonio Express',
+        nombre: 'Testimonio ',
+        secondNombre: 'Express',
         descripcion: 'Escribe un comentario y genera tarjetas de testimonios personalizadas .',
         imagen: 'https://assets.roadmap.sh/guest/testimonials-min-3j2j4.png',
         link: '/testimonioCard',
+        svg :'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"  fill="currentColor"  class="feather feather-telegram">'
+        + '<path fill="currentColor" d="M21.5 2.5L2.6 10.4c-.6.2-.6.6-.2.8l4.3 1.7 1.6 5.3c.2.6.6.7 1 .5l2.3-1.9 4.4 3.3c.5.4 1 .1 1.1-.6L22 3.5c.2-.7-.4-1.2-1-1zm-4.7 5.3l-7.3 6.6-.3 3.6-1.3-4.5 8.9-5.7z"/>'
+        + '</svg>', 
     },
     {
         id: 3,
-        nombre: 'Proximo ',
+        nombre: 'Proximo',
+        secondNombre: 'Proyecto',
         descripcion: '-----',
         imagen: 'https://assets.roadmap.sh/guest/testimonials-min-3j2j4.png',
         link: '/testimonioCard',
-    },
+        svg: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">'
+           + '<path d="M3 12l1.41 1.41L11 6.83v10.59h2V6.83l6.59 6.58L21 12l-9-9-9 9z"/>'
+           + '</svg>',
+      },
     
 
 ]
