5ddc8c29993f60c0c01ba3bc4d056d89e9e489f0
z6nc <andresvelasquezarotinco@gmail.com>
2025-04-17 18:29:48 -0500

    feat : add pnpm

--- src/scripts/proyect.js
+++ src/scripts/proyect.js
@@ -66,7 +66,7 @@ export const Proyectos = [
         nombre : 'Triada',
         secondNombre : 'Dinámica',
         descripcion : 'Juega al clásico juego piedra , papel y tijera  , el juego esta disponible para dos jugadores ',
-        imagen : 'https://res.cloudinary.com/dleem7osr/image/upload/v1741220439/TickTack_b6ofbf.webp',
+        imagen : '',
         link : '/Triada',
         svg : '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 24 24">'
         + '<path d="M6 6h5v5H6V6zm0 7h5v5H6v-5zm0 7h5v5H6v-5zm7-14h5v5h-5V6zm0 7h5v5h-5v-5zm0 7h5v5h-5v-5zm7-14h5v5h-5V6zm0 7h5v5h-5v-5zm0 7h5v5h-5v-5z"/>'
